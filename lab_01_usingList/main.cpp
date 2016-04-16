35293c2cdccee14897bd7811be52a82de77fed63
Kitsune90 <cristianocavo@icloud.com>
2016-04-16 14:34:26 +0200

    Initial Commit

---
+++ lab_01_usingList/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  lab_01_usingList
+//
+//  Created by Cristiano Cavo on 16/04/16.
+//  Copyright © 2016 Cristiano Cavo. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
